a0079de59958af4a1d0b2d7fd73a7b082ec3bf06
Prachi Modi <prachi.modi142@gmail.com>
2020-09-25 19:04:39 +0530

    Initial Commit

---
+++ IGClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IGClone
+//
+//  Created by Prachi Modi on 9/25/20.
+//  Copyright © 2020 Prachi Modi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
