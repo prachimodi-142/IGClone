894952bec8629997b67d7e46882f2983a1f5030e
Prachi Modi <prachi.modi142@gmail.com>
2020-09-28 17:22:31 +0530

    Third Commit

--- IGClone/Controllers/Other/Onboarding/LoginViewController.swift
+++ IGClone/Controllers/Other/Onboarding/LoginViewController.swift
@@ -181,11 +181,32 @@ class LoginViewController: UIViewController {
         
         guard let usernameEmail = usernameEmailField.text, !usernameEmail.isEmpty,
             let password = passwordField.text, !password.isEmpty, password.count >= 8
-        else {
-            return
+            else {
+                return
         }
         //login
-
+        var username: String?
+        var email: String?
+        
+        if usernameEmail.contains("@"), usernameEmail.contains(".") {
+            email = usernameEmail
+        }
+        else {
+            username = usernameEmail
+        }
+        AuthManager.shared.loginNewUser(username: username, email: email, password: password) { success in
+            DispatchQueue.main.async {
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                }
+                else {
+                    let alert = UIAlertController(title: "Log In Error", message: "We were unable to log you in.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                    self.present(alert, animated: true)
+                }
+            }
+        }
+        
     }
     
     @objc private func didTapTermsButton() {
@@ -205,7 +226,8 @@ class LoginViewController: UIViewController {
     
     @objc private func didTapCreateAccountButton() {
         let vc = RegistrationViewController()
-        present(vc, animated: true)
+        vc.title = "Create Account"
+        present(UINavigationController(rootViewController: vc), animated: true)
     }
     
     
--- IGClone/Resources/AuthManager.swift
+++ IGClone/Resources/AuthManager.swift
@@ -11,10 +11,45 @@ import FirebaseAuth
 public class AuthManager {
     static let shared = AuthManager()
     
-    public func registerNewUser(username: String, email: String, password: String) {
+    public func registerNewUser(username: String, email: String, password: String, completion : @escaping (Bool) -> Void) {
+        
+        DatabaseManager.shared.canCreateNewUser(with: email, username: username) { canCreate in
+            if canCreate {
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    guard error == nil, result != nil else {
+                        completion(false)
+                        return
+                    }
+                    DatabaseManager.shared.insertNewUser(with: email, username: username) { inserted in
+                        if inserted {
+                            completion(true)
+                            return
+                        }
+                        else{
+                            completion(false)
+                            return
+                        }
+                    }
+                }
+            }
+            else {
+                completion(false)
+            }
+        }
         
     }
-    public func loginNewUser(username: String?, email: String?, password: String){
-        
+    public func loginNewUser(username: String?, email: String?, password: String, completion: @escaping (Bool) -> Void) {
+        if let email = email {
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                guard authResult != nil, error == nil else {
+                    completion(false)
+                    return
+                }
+                completion (true)
+            }
+        }
+        else if let username = username {
+            print(username)
+        }
     }
 }
